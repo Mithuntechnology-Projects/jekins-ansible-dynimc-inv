4efa7bf71605ffcaa4f97d54005da54e207a424e
prasadambati1 <139728981+prasadambati1@users.noreply.github.com>
2023-09-09 18:22:55 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -2,6 +2,7 @@ provider "aws"{
     region = "ap-south-1"
 }
 resource "aws_instance" "AWSServer"{
+    count = 5
     ami = "ami-02bb7d8191b50f4bb"
     instance_type = "t2.micro"
     security_groups = ["launch-wizard-11"]

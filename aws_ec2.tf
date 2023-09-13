47cbd0b3303aa9ad19275b4909e6bc68aeceb47e
prasadambati1 <139728981+prasadambati1@users.noreply.github.com>
2023-09-13 23:24:28 +0530

    Update aws_ec2.tf

--- aws_ec2.tf
+++ aws_ec2.tf
@@ -1,13 +1,15 @@
-provider "aws"{
+provider "aws" {
     region = "ap-south-1"
 }
-resource "aws_instance" "AWSServer"{
-    count = 5
-    ami = "ami-05552d2dcf89c9b24"
-    instance_type = "t2.micro"
+
+resource "aws_instance" "AWSServer" {
+    count          = 5
+    ami            = "ami-05552d2dcf89c9b24"
+    instance_type  = "t2.micro"
     security_groups = ["launch-wizard-11"]
-    key_name = "devopsprasad"
-tags = {
-    Name = "tomcatservers"
-}
+    key_name       = "devopsprasad"
+
+    tags = {
+        Name = "tomcatservers"
+    }
 }
